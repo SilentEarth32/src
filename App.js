5f187943e6df107db079e181fdcf01f6b787eb99
SilentEarth32 <stephenmg3250@gmail.com>
2021-11-23 21:55:34 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,12 @@
+function App() {
+const name = 'Brad'
+
+  return (
+    <div className='container'>
+      <h1>Hello From {name}</h1>
+      <h2>{1+1}</h2>
+    </div>
+  );
+}
+
+export default App;
