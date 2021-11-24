eebc53e583436c0bc7f24edae94d800969a504d0
SilentEarth32 <stephenmg3250@gmail.com>
2021-11-24 15:11:37 -0600

    first real commit

--- App.js
+++ App.js
@@ -5,8 +5,10 @@ const name = 'Brad'
     <div className='container'>
       <h1>Hello From {name}</h1>
       <h2>{1+1}</h2>
+      <h3>testing testing</h3>
     </div>
   );
 }
 
 export default App;
+
